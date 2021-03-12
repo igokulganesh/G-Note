e88ea1ae5841267c497682a980065c20c21ba8df
Gokul Ganeshan I <igokulganesh@gmail.com>
2021-03-12 14:25:04 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,5 @@
 function init() 
 {
-
-  // Your web app's Firebase configuration
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const config = {
     apiKey: "AIzaSyBihDOYXAqvOayAPXCbqIudZZCogzikx08",
